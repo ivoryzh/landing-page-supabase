4d479ec2fffb8e30d27abd4821bd34b90eb0321d
ivoryzhangwy <ivoryzhangwy@gmail.com>
2025-12-02 13:40:38 -0800

    move optimizers and port to ivoryos config,

--- app/hub/page.tsx
+++ app/hub/page.tsx
@@ -32,7 +32,7 @@ export default async function ProtectedPage() {
     <div className="flex-1 w-full flex flex-col gap-6 p-4 max-w-7xl mx-auto">
       {/* The Main Hub Application */}
       <div className="flex flex-col gap-4">
-        <IvoryOSHub userEmail={userEmail} modules={modules || []} />
+        <IvoryOSHub userEmail={userEmail || null} modules={modules || []} />
       </div>
     </div>
   );
