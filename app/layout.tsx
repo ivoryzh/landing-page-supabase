4870b2a0c59628b68d173bbcf286366940d4eba2
ivoryzhang <ivoryzhangwy@gmail.com>
2026-01-15 20:55:48 -0800

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,54 @@ const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
+const title = "IvoryOS - Self-Driving Labs in One Line";
+const description = "The plug-and-play orchestrator for rapid lab automation. Built for scientists, by scientists.";
+
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "IvoryOS - Self-Driving Labs in One Line",
-  description: "The modular platform for rapid hardware and analysis development.",
+  title: {
+    default: title,
+    template: "%s | IvoryOS",
+  },
+  description,
+  keywords: [
+    "Lab Automation",
+    "Python",
+    "Science",
+    "Robotics",
+    "Self-Driving Labs",
+    "Chemistry",
+    "Biology",
+    "Research",
+    "Open Source",
+  ],
+  authors: [
+    {
+      name: "IvoryOS Team",
+      url: "https://ivoryos.ai",
+    },
+  ],
+  creator: "IvoryOS Team",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: defaultUrl,
+    title,
+    description,
+    siteName: "IvoryOS",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "IvoryOS Landing Page",
+      },
+    ],
+  },
+
+  alternates: {
+    canonical: defaultUrl,
+  },
   icons: {
     icon: "/favicon.ico",
   },
